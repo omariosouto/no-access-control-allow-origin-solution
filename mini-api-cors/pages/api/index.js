d10dd5bdbb3fddde6b3975fb4736fb58d741781c
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-02-03 07:07:14 -0300

    update

---
+++ mini-api-cors/pages/api/index.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+    res.status(200).json({
+        platform: 'Back End',
+        name: 'Mario',
+        message: 'DESCONFIGURA O COTONETE EM CIMA DO BOTÃO DE LIKE'
+    })
+}
